e4cb0618127e202fe99d32ae3b7da85b4a119cb4
atharvaishere <87153650+atharvaishere@users.noreply.github.com>
2025-07-29 18:23:03 +0100

    Add EditProject route to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,26 +10,26 @@ import Project from './pages/Project';
 import ProjectList from './pages/ProjectList';
 import ProjectForm from './pages/ProjectForm';
 import Search from './pages/Search';
+import EditProject from './pages/EditProject';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        
-          <div className="min-h-screen bg-gradient-to-br from-background to-gray-100 dark:from-darkBg dark:to-gray-800">
-            <Navbar />
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/project/:id" element={<Project />} />
-              <Route path="/projects" element={<ProjectList />} />
-              <Route path="/project/create" element={<ProjectForm />} />
-              <Route path="/search" element={<Search />} />
-            </Routes>
-          </div>
-        
+        <div className="min-h-screen bg-gradient-to-br from-background to-gray-100 dark:from-darkBg dark:to-gray-800">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/project/:id" element={<Project />} />
+            <Route path="/projects" element={<ProjectList />} />
+            <Route path="/project/create" element={<ProjectForm />} />
+            <Route path="/project/edit/:id" element={<EditProject />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </div>
       </Router>
     </AuthProvider>
   );
